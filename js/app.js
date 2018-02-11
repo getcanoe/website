71bc0439264776f4d53e2d772ca8e01d548ba289
Mikehrn <mike.tasset@gmail.com>
2018-02-11 23:54:27 +0900

    Added mobile version
    Adjust most modules to fit on mobile, mobile 90% finished

--- js/app.js
+++ js/app.js
@@ -9,4 +9,14 @@ $(document).ready(function() {
     $('.android-download-link').show();
     $('.ios-download-link').show();
   }
+
+  $(".feature-list__feature").on("click", function() {
+    var feature = $(this).data("id");
+    var src = "assets/img/home/screens/screen-" + feature + ".jpg";
+    console.log(feature);
+    console.log(src);
+    $("#phone-screen").attr("src", src);
+    $(".feature-list__feature").removeClass("active");
+    $(this).addClass("active");
+  });
 });
