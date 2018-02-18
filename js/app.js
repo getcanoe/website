49d9e2376cdc97f503e2b966bd939e64e9d2be20
Mikehrn <mike.tasset@gmail.com>
2018-02-18 17:36:57 +0900

    Add old CSS for blog

--- js/app.js
+++ js/app.js
@@ -20,11 +20,35 @@ $(document).ready(function() {
     e.preventDefault();
   });
 
+  var i = 2;
+
+  if ($(window).width() > 1020) {
+    var timer = setInterval(function() {
+      $(".feature-list__feature").removeClass("active");
+      $(".phone__screen").removeClass("active");
+      $('.line').attr('class', 'line');
+      $('.feature-list__feature[data-id=' + i + ']').addClass('active');
+      $('.phone__screen[data-id=' + i + ']').addClass('active');
+      $('#line-' + i).attr('class', 'line active');
+  
+      if (i > 2) {
+        i = 1;
+      } else {
+        i++;
+      }
+    }, 4000);  
+  }
+
+
   $(".feature-list__feature").on("click", function() {
     var feature = $(this).data("id");
     $(".feature-list__feature").removeClass("active");
     $(".phone__screen").removeClass("active");
+    $('.line').attr('class', 'line');
     $(this).addClass("active");
     $('.phone__screen[data-id=' + feature + ']').addClass('active');
+    $('#line-' + feature).attr('class', 'line active');
+
+    clearInterval(timer);
   });
 });
